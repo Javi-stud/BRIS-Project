1c867430b9c9b3369534ac7ddff232a6429180c6
Javi-stud <Singcojayvince30@gmail.com>
2025-10-30 14:42:29 +0800

    Add files via upload

---
+++ ResidentsRecord/script.js
@@ -0,0 +1,16 @@
+cont residentsList = [
+  {
+  lastName:"Sing",
+  firstName:"Javi",
+  address:"Ginatilan",
+  age:16,
+},
+{
+  lastName:"ris",
+  firstName:"ding",
+  address:"argao",
+  age:17,
+},
+  ];
+
+function renderResidents (data)
