13e66dba674da3e143ddf0e585f2769bcc33e4c7
Javi-stud <Singcojayvince30@gmail.com>
2025-10-31 13:13:35 +0800

    Refactor residents list and add styling
    Removed hardcoded residents list and added CSS styles for layout.

--- ResidentsRecord/script.js
+++ ResidentsRecord/script.js
@@ -1,43 +1,99 @@
-const residentsList = [
-  {
-  lastName:"Sing",
-  firstName:"Javi",
-  address:"Ginatilan",
-  age:16,
-},
-{
-  lastName:"ding",
-  firstName:"risl",
-  address:"argao",
-  age:17,
-},
-  ];
-
-function renderResidents () {
-  const boxInfo = document.getElementsByClassName('residentsList')[0];  boxInfo.innerHTML='';
-  
-  residentsList.forEach(resident => {
-    
-    const info = document.createElement('div');
-    info.classList.add('residentsInfo');
-    
-    const name = document.createElement('div');
-    name.classList.add('residentsName');
-    name.textContent = ` ${resident.firstName} ${resident.lastName} `;
-    
-    const address= document.createElement('div');
-    address.classList.add('residentsAddress');
-    address.textContent = `address: ${resident.address} - age: ${resident.age}`;
-    
-    
-    info.append(name, address);
-    
-    boxInfo.append(info);
-    
-  });
+body {
+  margin:0;
+  padding:0;
+  cursor:default;
+  user-select:none;
+  background:#F9FAFB;
+  font-family:sans-serif;
 }
 
-renderResidents();
+#header {
+  gap:65.5%;
+  width:100%;
+  height:66px;
+  display:flex;
+  padding:.1px;
+  background:#208AAE;
+  box-sizing:border-box;
+}
+
+#header .residentp {
+  margin:0;
+  padding-top:1%;
+  padding-bottom:1%;
+  color:#FFFFFF;
+  margin-left:2%;
+  font-size:27px;
+}
+
+#header .back {
+  font-size:22px;
+  color:white;
+  margin-left:14%;
+  margin-top:22px;
+  cursor:pointer;
+}
+
+#searchbox {
+  display:flex;
+  justify-content:center;
+  margin-top:3%;
+  margin-left:7%;
+  margin-right:7%;
+  padding:15px;
+  border:1px solid #D1D5DB;
+  box-shadow:0 0 5px rgba(0, 0, 0, 0.2);
+  border-radius:9px;
+}
+
+.search {
+  width:98%;
+  height:38px;
+  font-size:15px;
+  border:1px solid #D1D5DB;
+  box-shadow:0 0 4px rgba(0, 0, 0, 0.3);
+  border-radius:4px;
+}
+
+.serbot {
+  background:#0D2149;
+  margin-left:10px;
+  height:41px;
+  width:45px;
+  border-radius:6px;
+}
+
+.serbot img {
+  filter: invert(1);
+}
+
+.namelist {
+  display:flex;
+  list-style:none;
+  
+}
 
 
-                        
+.residentsList {
+  display:flex;
+  align-items:center;
+  flex-direction:column;
+  justify-content:center;
+}
+
+#residentsListBox {
+  padding: 1.1% 1.5%;
+  border:1px solid blue;
+  width:82.9%;
+  margin-left:7%;
+  border-radius:10px;
+  margin-top:2%;
+}
+
+.residentsInfo {
+  padding:10px;
+  border:1px solid red;
+  border-radius:3px;
+  width:100%;
+}
+
