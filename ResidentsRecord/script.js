be7866d12f51cda6b493a53841e5201510c6b509
Javi-stud <Singcojayvince30@gmail.com>
2025-10-31 13:13:19 +0800

    Update script.js

--- ResidentsRecord/script.js
+++ ResidentsRecord/script.js
@@ -1,4 +1,4 @@
-cont residentsList = [
+const residentsList = [
   {
   lastName:"Sing",
   firstName:"Javi",
@@ -6,11 +6,38 @@ cont residentsList = [
   age:16,
 },
 {
-  lastName:"ris",
-  firstName:"ding",
+  lastName:"ding",
+  firstName:"risl",
   address:"argao",
   age:17,
 },
   ];
 
-function renderResidents (data)
+function renderResidents () {
+  const boxInfo = document.getElementsByClassName('residentsList')[0];  boxInfo.innerHTML='';
+  
+  residentsList.forEach(resident => {
+    
+    const info = document.createElement('div');
+    info.classList.add('residentsInfo');
+    
+    const name = document.createElement('div');
+    name.classList.add('residentsName');
+    name.textContent = ` ${resident.firstName} ${resident.lastName} `;
+    
+    const address= document.createElement('div');
+    address.classList.add('residentsAddress');
+    address.textContent = `address: ${resident.address} - age: ${resident.age}`;
+    
+    
+    info.append(name, address);
+    
+    boxInfo.append(info);
+    
+  });
+}
+
+renderResidents();
+
+
+                        
