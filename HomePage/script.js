79c1cab11fb7a1bc6da9e7d25e949a53775b8ed3
Javi-stud <Singcojayvince30@gmail.com>
2025-11-05 05:58:29 -1200

    Update script.js

--- HomePage/script.js
+++ HomePage/script.js
@@ -10,5 +10,25 @@ Menubar.addEventListener("click", () => {
   }
 });
 
+const logoutbutton = document.getElementById("logoutbutton");
+const logoutconfirm = document.getElementById("logoutconfirm");
+const cancel = document.getElementById("cancel");
+const logout = document.getElementById("logout");
 
+logoutbutton.addEventListener("click", () => {
+logoutconfirm.style.display = "flex";
+});
 
+cancel.addEventListener("click", () => {
+logoutconfirm.style.display = "none";
+});
+
+logout.addEventListener("click", () => {
+window.location.href = "/login";
+});
+
+window.addEventListener("click", (e) => {
+if (e.target === logoutconfirm) {
+logoutconfirm.style.display = "none";
+}
+});
